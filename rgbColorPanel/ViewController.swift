b77dc96896aaf566414c0854e8c76f1198fbd13f
SerVas <Smith1477@yandex.ru>
2021-12-26 18:36:19 +0300

    corrections. homework errors

--- rgbColorPanel/ViewController.swift
+++ rgbColorPanel/ViewController.swift
@@ -11,46 +11,62 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var colorView: UIView!
     
-    @IBOutlet weak var redSlider: UISlider! {
-        didSet {
-            redSlider.tintColor = UIColor.red
-        }
-    }
-    @IBOutlet weak var greenSlider: UISlider! {
-        didSet {
-            greenSlider.tintColor = UIColor.green
-        }
-    }
-    @IBOutlet weak var blueSlider: UISlider! {
-        didSet {
-            blueSlider.tintColor = UIColor.blue
-        }
-    }
-    
     @IBOutlet weak var redLabel: UILabel!
     @IBOutlet weak var greenLabel: UILabel!
     @IBOutlet weak var blueLabel: UILabel!
     
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        colorView.layer.cornerRadius = 10
+        
+        colorView.layer.cornerRadius = 15
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        
+        setColor()
+        setValue(for: redLabel, greenLabel, blueLabel)
     }
-    func colorPanelRGB() {
-        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    
+    
+    @IBAction func numberRedSlider(_ sender: UISlider) {
+        setColor()
+        
+        switch sender {
+        case redLabel: setValue(for: redLabel)
+        case greenLabel: setValue(for: greenLabel)
+        default: setValue(for: blueLabel)
+        }
+        
     }
-    @IBAction func numberRedSlider(_ sender: Any) {
-        colorPanelRGB()
-        redLabel.text = String(redSlider.value)
-        redLabel.text = String(round(1000 * redSlider.value) / 1000)
+    private func setColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
     }
-    @IBAction func numberGreenSlider(_ sender: Any) {
-        colorPanelRGB()
-        greenLabel.text = String(greenSlider.value)
-        greenLabel.text = String(round(1000 * greenSlider.value) / 1000)
+    
+    
+    
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label {
+            case redLabel:
+                label.text = string(from: redSlider)
+            case greenLabel:
+                label.text = string(from: greenSlider)
+            case blueLabel:
+                label.text = string(from: blueSlider)
+            }
+        }
     }
-    @IBAction func numberBiueSlider(_ sender: Any) {
-        colorPanelRGB()
-        blueLabel.text = String(blueSlider.value)
-        blueLabel.text = String(round(100 * blueSlider.value) / 100)
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
     }
 }
